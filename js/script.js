9341601a7e0f6a9e4cb9c1285b774a50dace2f7f
shreyansdjp <gamingeyefinity@gmail.com>
2017-12-05 02:20:47 +0530

    Almost Done

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857"];
-
+var actualQuote, author;
 
 function getQuote() {
+    console.log('clicked');
     $.ajax({
         type: "POST",
         headers: {
@@ -11,11 +12,13 @@ function getQuote() {
         },
         url: "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=movies&count=1",
         success: function(result) {
+            json = JSON.parse(result);
             var color = Math.floor(Math.random() * colors.length);
-            $('.quote').text(result.quote);
-            $('.author').text(result.author);
+            $('.quote').html(json.quote);
+            actualQuote = json.quote;
+            $('.author').html(json.author);
+            author = json.author;
             $("html body").css("background-color", colors[color]);
-            console.log(result);
         },
         error: function(e) {
             console.log(e);
@@ -25,4 +28,8 @@ function getQuote() {
 
 $(document).ready(function() {
     getQuote();
+    $('#next').on('click', getQuote);
+    $('#twitter-button').on('click', function() {
+        $(this).attr('href', 'https://twitter.com/intent/tweet?text=' + actualQuote + "-By " + author)
+    });
 });
